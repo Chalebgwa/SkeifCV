aab1c2989d646c3e8f5bddcf7efe41a59b8780bb
Chalebgwa <blackcrown.bc@gmail.com>
2025-11-10 14:55:10 +0200

    Auto commit: 2025-11-10 12:55:10Z

--- lib/models/education.dart
+++ lib/models/education.dart
@@ -10,4 +10,39 @@ class Education {
     this.dates = '',
     this.description = '',
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'institution': institution,
+      'degree': degree,
+      'dates': dates,
+      'description': description,
+    };
+  }
+
+  factory Education.fromJson(Map<String, dynamic>? json) {
+    if (json == null) {
+      return Education();
+    }
+    return Education(
+      institution: json['institution'] ?? '',
+      degree: json['degree'] ?? '',
+      dates: json['dates'] ?? '',
+      description: json['description'] ?? '',
+    );
+  }
+
+  Education copyWith({
+    String? institution,
+    String? degree,
+    String? dates,
+    String? description,
+  }) {
+    return Education(
+      institution: institution ?? this.institution,
+      degree: degree ?? this.degree,
+      dates: dates ?? this.dates,
+      description: description ?? this.description,
+    );
+  }
 }
